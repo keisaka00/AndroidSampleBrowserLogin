03cf5bfac2cc88aeb3a360f44f36238843c8abc2
坂本圭一郎 <keisaka00@gmail.com>
2022-12-03 22:09:33 +0900

    ボタンの追加とviewBindingの対応

--- app/src/main/java/com/example/samplebrowserlogin/MainActivity.kt
+++ app/src/main/java/com/example/samplebrowserlogin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.samplebrowserlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.example.samplebrowserlogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "LOGIN"
+
+    private lateinit var binding: ActivityMainBinding
+
+    /**
+     * アプリ新規起動時に呼ばれる
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // ログインボタンの動作
+        binding.btnLogin.setOnClickListener {
+        }
     }
 }
